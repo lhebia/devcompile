db1452381047ebf0b6130dbb88c5fbac3018ab36
lhebia <lhebia@gmail.com>
2020-06-23 14:56:19 -0400

    Updates

---
+++ precache-manifest.f2d41b82314f2d8259020e51782804d0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e03d2c769d998079ecc6a5dbf3baa789",
+    "url": "/lawrence-hebia-project-five/index.html"
+  },
+  {
+    "revision": "8fe6a13a31a066cb2425",
+    "url": "/lawrence-hebia-project-five/static/css/main.53bea5eb.chunk.css"
+  },
+  {
+    "revision": "6f382587250e9c819365",
+    "url": "/lawrence-hebia-project-five/static/js/2.ab6dde8f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lawrence-hebia-project-five/static/js/2.ab6dde8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fe6a13a31a066cb2425",
+    "url": "/lawrence-hebia-project-five/static/js/main.562b802c.chunk.js"
+  },
+  {
+    "revision": "196db662b46962054aeb",
+    "url": "/lawrence-hebia-project-five/static/js/runtime-main.fd9583c8.js"
+  },
+  {
+    "revision": "f8dffb3aef00d09ede4399e99f671adc",
+    "url": "/lawrence-hebia-project-five/static/media/about.f8dffb3a.png"
+  },
+  {
+    "revision": "f847defb6122536c6e57076c6b7431eb",
+    "url": "/lawrence-hebia-project-five/static/media/splash.f847defb.png"
+  }
+]);
